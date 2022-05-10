5054e4e825b64dfc897a5e8711858aac1d1ad6b1
aster-alemu <leaster2014w@gmail.com>
2022-05-10 11:52:28 -0700

    Add styles to the menu

---
+++ main.js
@@ -0,0 +1,9 @@
+// let menuIcon = document.querySelector('#menu-icon').onclick = function() {
+  
+//   //document.querySelector("#menu-item").style.display = "block";
+//  let div = document.querySelector('#top-section');
+// div.classList.replace('#menu-item');
+
+// }
+// menuIcon.addEventListener('click',display);
+
